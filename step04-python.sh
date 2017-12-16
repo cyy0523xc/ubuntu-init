f41d432f2cc97d89fa1d49bc97984275fe9c8f11
Alex Cai <cyy0523xc@gmail.com>
2017-12-17 00:15:55 +0800

    py go

---
+++ step04-python.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# 
+# python基础安装配置
+# Author: alex
+# Created Time: 2017年12月17日 星期日 00时13分13秒
+
---
+++ step05-golang.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# 
+# golang环境安装与配置
+# Author: alex
+# Created Time: 2017年12月17日 星期日 00时13分44秒
+
