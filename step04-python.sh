476d5a8e138c27cb37aa037c56a1e9a3d3d4290f
Alex Cai <cyy0523xc@gmail.com>
2017-12-19 22:50:51 +0800

    python

--- step04-python.sh
+++ step04-python.sh
@@ -4,3 +4,11 @@
 # Author: alex
 # Created Time: 2017年12月17日 星期日 00时13分13秒
 
+sudo apt-get install -y ipython3 python3-pip
+
+sudo -H pip3 install -U pip \
+    numpy \
+    pandas \
+    jieba \
+    scipy \
+    nltk
