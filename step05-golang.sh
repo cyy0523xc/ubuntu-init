afacbdca26bfbc67bb5d4b760b9b87271a0cee60
Alex Cai <cyy0523xc@gmail.com>
2018-01-02 22:43:57 +0800

    golang

--- step05-golang.sh
+++ step05-golang.sh
@@ -118,6 +118,27 @@ else
     echo "往文件$bashrc 和 $zshrc加入了内容"
 fi
 
+# 配置golang
+mkdir -p /var/www/src/golang.org/x/
+cd /var/www/src/golang.org/x/
+git clone https://github.com/golang/tools
+git clone https://github.com/golang/text
+git clone https://github.com/golang/crypto
+
+# vim-go
+go get golang.org/x/tools/cmd/guru
+go get golang.org/x/tools/cmd/goimports
+go get golang.org/x/tools/cmd/gorename
+go get github.com/golang/lint/golint
+go get github.com/zmb3/gogetdoc
+go get github.com/fatih/motion
+go get github.com/kisielk/errcheck
+go get github.com/nsf/gocode
+go get github.com/jstemmer/gotags
+go get github.com/josharian/impl
+go get github.com/fatih/gomodifytags
+go get github.com/dominikh/go-tools/cmd/keyify
+
 echo "ok"
 echo "查看go版本的命令：go version"
 echo "查看相关环境变量：go env"
