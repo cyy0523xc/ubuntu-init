5ac2d23322923fe427cf17a2ff3712a37603669e
alex <cyy0523xc@gmail.com>
2018-04-14 16:03:41 +0800

    java

---
+++ step06-java-tools.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 
+# java相关工具
+# Author: alex
+# Created Time: 2018年04月14日 星期六 14时00分15秒
+
+# 新词发现算法
+wget https://github.com/sing1ee/dict_build/blob/master/dict_build-0.0.3.tar?raw=true -O /var/www/dict_build-0.0.3.tar
+
+
+
