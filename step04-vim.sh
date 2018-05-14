194b7be00088ff631afc147fa9cf13f8fe42c857
alex <cyy0523xc@gmail.com>
2018-05-14 22:32:21 +0800

    gvim

--- step04-vim.sh
+++ step04-vim.sh
@@ -33,5 +33,14 @@ install bash install-YoucompleteMe.sh
 install bash install.sh
 install bash install-color.sh
 
+# install vim-anywhere
+echo "Install vim-anywhere..."
+sudo apt-get install xclip
+curl -fsSL https://raw.github.com/cknadler/vim-anywhere/master/install | bash
+# ~/.vim-anywhere/update
+echo "如果ctrl+alt+v快捷键不起作用，则需要在系统设置->键盘里手动设置快捷键，执行脚本："
+echo "$HOME/.vim-anywhere/bin/run"
+echo "使用的时候，先ctrl+alt+v调出gvim进行编辑，编辑好之后，:wq退出保存到缓冲区，然后使用ctrl+v粘贴即可。"
+
 cd -
 echo "install ok."
