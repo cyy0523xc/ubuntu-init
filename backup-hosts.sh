c95ea99d3569bbec27e0c9c2926c5c841b37aa9e
alex <cyy0523xc@gmail.com>
2018-06-04 10:00:44 +0800

    backup and recover hosts

--- backup-hosts.sh
+++ backup-hosts.sh
@@ -5,6 +5,7 @@
 # Author: alex
 # Created Time: 2017年12月18日 星期一 17时35分40秒
 
+echo "正在备份host文件："
 echo "密码使用姓名和QQ号相关的："
 openssl aes-128-cbc -salt -in /etc/hosts -out ./config/hosts
 
--- recover-hosts.sh
+++ recover-hosts.sh
@@ -4,9 +4,10 @@
 # Author: alex
 # Created Time: 2017年12月18日 星期一 17时53分35秒
 
+echo "[注意]正在恢复host文件:"
+echo "[提示]密码使用姓名和QQ号相关的:"
 output=./config/hosts-output
 if 
-    echo "[提示]密码使用姓名和QQ号相关的："
     openssl aes-128-cbc -d -salt -in ./config/hosts -out "$output"
 then
     sudo mv "$output" /etc/hosts
