07646ea14c5a26906d761b62b589c28b9a83f662
alex <cyy0523xc@gmail.com>
2018-07-05 20:58:50 +0800

    python

--- step03-python.sh
+++ step03-python.sh
@@ -4,6 +4,26 @@
 # Author: alex
 # Created Time: 2017年12月17日 星期日 00时13分13秒
 
+# install 最新版本Python
+version=3.6.6
+sudo aptitude -y install gcc make zlib1g-dev
+wget https://www.python.org/ftp/python/$version/Python-$version.tar.xz
+tar xJf Python-$version.tar.xz
+cd Python-$version
+./configure
+make
+make install
+
+# which python3
+v_msg=`python3 -V`
+r_msg="Python $version"
+if [ ! "$v_msg" = "$r_msg" ]; then 
+    echo "version is not right!"
+    echo "ln -s /usr/local/bin/python3.6 /usr/bin/python3"
+    exit 1
+fi
+
+# ipython
 sudo apt-get install -y ipython3 
 
 # pip
