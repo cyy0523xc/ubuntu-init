0296f8cd7b77860693192f9f2aa707864add2cda
alex cai <cyy0523xc@gmail.com>
2018-12-27 16:05:54 +0800

    python

--- step03-python-ml.sh
+++ step03-python-ml.sh
@@ -4,7 +4,22 @@
 # Author: alex
 # Created Time: 2017年12月17日 星期日 00时13分13秒
 
-sudo -H pip install scikit-learn
+sudo -H pip install \
+    pandas \
+    numexpr \
+    matplotlib \
+    scipy \
+    seaborn \
+    scikit-learn \
+    scikit-image \
+    sympy \
+    patsy \
+    statsmodel \
+    numba \
+    bokeh \
+    vincent \
+    xlrd \
+    Facets
 
 # 推荐系统
 sudo -H pip install lightfm
--- step03-python.sh
+++ step03-python.sh
@@ -23,7 +23,7 @@ else
 fi
 
 if [ $update = 1 ]; then
-    version=3.6.6
+    version=3.6.7
     sudo aptitude -y install gcc make zlib1g-dev
     wget https://www.python.org/ftp/python/$version/Python-$version.tar.xz
     tar xJf Python-$version.tar.xz
@@ -56,12 +56,21 @@ sudo -H pip install -U pip \
     jieba \
     scipy \
     nltk \
+    cython \
     sphinx \
     sphinx_rtd_theme \
     Cython \
     flask flask_restful \
     fire \
+    sqlalchemy \
+    hdf5 \
+    beautifulsoup \
+    protobuf \
+    cloudpickle \
+    dill \
     pyinstaller 
+
+sudo -H pip3 install git+https://github.com/ibbd-dev/python-fire-rest.git
 
 echo "请手动安装以下工具："
 echo "使用sql查询csv: https://github.com/harelba/q"
